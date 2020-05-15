0b57ee5a6909b2bba771d6f4781503193c88bd65
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-14 23:37:02 -0700

    refactor pt. 2

--- src/inheritance/MatchReader.ts
+++ src/inheritance/MatchReader.ts
@@ -1,4 +1,4 @@
-import { CsvFileReader } from './CsvFileReader';
+import { CsvFileReader } from './inheritance/CsvFileReader';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
