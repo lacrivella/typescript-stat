94a72e9254ffe46dca47c18d1a4d304b3341bb85
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-14 23:27:40 -0700

    cut MatchData tuple out of CsvFileReader and into MatchReader. That was CsvFileReader is more reusable

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,7 @@
 import fs from 'fs';
-import { MatchResult } from './MatchResult';
-
-// tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export abstract class CsvFileReader<T> {
   data: T[] = [];
-
   constructor(public filename: string) {}
   abstract mapRow(row: string[]): T;
 
