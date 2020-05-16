0636ae239bad305a1f6d068d56f3f524f2dd7401
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-15 17:27:04 -0700

    load method that reads data and parse the correct tuple structure

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -4,26 +4,31 @@ import { MatchResult } from './MatchResult';
 // tuple
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
+
   constructor(public reader: DataReader) {}
 
-  // .map(
-  //   (row: string[]): MatchData => {
-  //     return [
-  //       dateStringToDate(row[0]),
-  //       row[1],
-  //       row[2],
-  //       parseInt(row[3]),
-  //       parseInt(row[4]),
-  //       row[5] as MatchResult, //type assertion: 'H', 'A', 'D'
-  //       row[6],
-  //     ];
-  //   }
-  // );
+  load(): void {
+    this.reader.read();
+    // gives back a new array
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, //type assertion: 'H', 'A', 'D'
+          row[6],
+        ];
+      }
+    );
+  }
 }
