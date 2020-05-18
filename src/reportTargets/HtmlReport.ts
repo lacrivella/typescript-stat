e122828f3489d13fa8dd92f3c396a7e2b307cccf
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-18 13:02:40 -0700

    created basic html for the report to generate onto

---
+++ src/reportTargets/HtmlReport.ts
@@ -0,0 +1,12 @@
+import { OutputTarget } from '../Summary';
+
+export class HtmlReport implements OutputTarget {
+  print(report: string): void {
+    const html = `
+      <div>
+        <h1>Analysis Report</h1>
+        <div>${report}</div>
+      </div>
+    `;
+  }
+}
