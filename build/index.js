dfb1e9c38823971f78dd6d0fcda810aa9c579791
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-18 12:58:36 -0700

    putting it all together in index.ts. Able to generate a console report with Man United total wins

--- build/index.js
+++ build/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var MatchResult_1 = require("./MatchResult");
+var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+var Summary_1 = require("./Summary");
 // create an object that satisfies the DataReader interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // create an instance of MatchReader &
@@ -10,14 +12,5 @@ var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 // we can reference matchReader.matches after we invoke .load()
-var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log("Manchest United won " + manUnitedWins + " games this season!");
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // create an object that satisfies the DataReader interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -11,4 +13,9 @@ const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 // we can reference matchReader.matches after we invoke .load()
 
-console.log(`Manchest United won ${manUnitedWins} games this season!`);
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
